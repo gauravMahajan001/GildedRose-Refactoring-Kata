9001a49fe42c2a0061f69253b559d05270d2a06c
Dell <Dell@DESKTOP-9T5M8TE>
2023-06-23 07:02:42 -0700

    Reactor : Remove the unused code and call the factory method to update

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,10 +2,10 @@ package com.gildedrose;
 
 import java.util.Arrays;
 
-import com.gildedrose.constant.ApplicationConstant;
 import com.gildedrose.service.ItemUpdate;
+import com.gildedrose.service.ItemUpdateFactory;
 
-class GildedRose implements ItemUpdate {
+class GildedRose {
 	Item[] items;
 
 	public GildedRose(Item[] items) {
@@ -13,60 +13,11 @@ class GildedRose implements ItemUpdate {
 	}
 
 	public void updateQuality() {
+
 		Arrays.stream(items).forEach(item -> {
-			
-			if (item.name.equals(ApplicationConstant.AGED_BRIE) || item.name.equals(ApplicationConstant.BACKSTAGE)) {
-
-				if (item.quality < 50) {
-					this.increaseQuantity(item);
-
-					if (item.name.equals(ApplicationConstant.BACKSTAGE)) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-
-								this.increaseQuantity(item);
-							}
-						}
-
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								this.increaseQuantity(item);
-							}
-						}
-					}
-				}
-			} else {
-				if (item.quality > 0) {
-					if (!item.name.equals(ApplicationConstant.SULFURAS)) {
-						this.decreaseQuantity(item);
-					}
-				}
-			}
-
-			if (!item.name.equals(ApplicationConstant.SULFURAS)) {
-				this.decreaseSellin(item);
-			}
-
-			if (item.sellIn < 0) {
-				if (item.name.equals("Aged Brie")) {
-
-					if (item.quality < 50) {
-						this.increaseQuantity(item);
-					}
-				} else {
-					if (item.name.equals(ApplicationConstant.BACKSTAGE)) {
-
-						item.quality = 0;
-					} else {
-						if (item.quality > 0) {
-							if (!item.name.equals(ApplicationConstant.SULFURAS)) {
-								this.decreaseQuantity(item);
-							}
-						}
-					}
-
-				}
-			}
+			ItemUpdate itemUpdate = ItemUpdateFactory.createItemUpate(item);
+			itemUpdate.update(item);
 		});
 	}
+
 }
